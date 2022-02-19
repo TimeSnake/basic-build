ae2d1c2669cb3f021911f1e65309e8d45e42cd4b
funzter <cavehive@protonmail.com>
2022-02-19 18:08:17 +0100

    changed channel listener

---
+++ src/main/java/de/timesnake/extension/build/server/EventManager.java
@@ -0,0 +1,26 @@
+package de.timesnake.extension.build.server;
+
+import de.timesnake.basic.bukkit.util.Server;
+import de.timesnake.extension.build.main.ExBuild;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+public class EventManager implements Listener {
+
+    public EventManager() {
+        Server.registerListener(this, ExBuild.getPlugin());
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent e) {
+        e.setYield(0);
+    }
+
+    @EventHandler
+    public void onBlockExplode(BlockExplodeEvent e) {
+        e.setYield(0);
+    }
+
+}
