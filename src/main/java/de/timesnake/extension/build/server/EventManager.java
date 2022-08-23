40bbe2e1239725cc67bc469096debd6b6994cf8e
funzter <cavehive@protonmail.com>
2022-08-24 00:11:08 +0200

    adapted world creation, added default world game rules

--- src/main/java/de/timesnake/extension/build/server/EventManager.java
+++ src/main/java/de/timesnake/extension/build/server/EventManager.java
@@ -1,7 +1,14 @@
 package de.timesnake.extension.build.server;
 
 import de.timesnake.basic.bukkit.util.Server;
+import de.timesnake.basic.bukkit.util.user.event.UserJoinEvent;
+import de.timesnake.extension.build.chat.Plugin;
 import de.timesnake.extension.build.main.ExBuild;
+import de.timesnake.library.basic.util.chat.ExTextColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockExplodeEvent;
@@ -21,6 +28,15 @@ public class EventManager implements Listener {
     @EventHandler
     public void onBlockExplode(BlockExplodeEvent e) {
         e.setYield(0);
+    }
+
+    @EventHandler
+    public void onUserJoin(UserJoinEvent e) {
+        e.getUser().sendPluginMessage(Plugin.BUILD, Component.text("Use ", ExTextColor.PERSONAL)
+                .append(Component.text("/mw tp ", ExTextColor.VALUE, TextDecoration.UNDERLINED)
+                        .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, "/mw tp "))
+                        .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Click to copy"))))
+                .append(Component.text("to teleport to a world", ExTextColor.PERSONAL)));
     }
 
 }
