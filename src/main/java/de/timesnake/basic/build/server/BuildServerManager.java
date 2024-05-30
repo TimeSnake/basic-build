fe56a40d1294796b339fce2311bdf2bf2d3a5ea4
funzter <funzter@mail.timesnake.de>
2024-05-30 19:01:24 +0200

    add waiting games

--- src/main/java/de/timesnake/basic/build/server/BuildServerManager.java
+++ src/main/java/de/timesnake/basic/build/server/BuildServerManager.java
@@ -8,6 +8,7 @@ import de.timesnake.basic.bukkit.util.ServerManager;
 import de.timesnake.basic.bukkit.util.world.WorldManager;
 import de.timesnake.database.util.server.DbBuildServer;
 import de.timesnake.library.basic.util.Status;
+import de.timesnake.library.waitinggames.WaitingGameManager;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 
@@ -17,12 +18,17 @@ public class BuildServerManager extends ServerManager {
     return (BuildServerManager) ServerManager.getInstance();
   }
 
+  private WaitingGameManager waitingGameManager;
+
   public void onBuildEnable() {
     Bukkit.setDefaultGameMode(GameMode.CREATIVE);
+
+    this.waitingGameManager = new WaitingGameManager();
   }
 
   public void onBuildDisable() {
     ((DbBuildServer) this.getDatabase()).clearWorlds();
+    this.waitingGameManager.onDisable();
   }
 
   @Override
@@ -33,5 +39,9 @@ public class BuildServerManager extends ServerManager {
   @Override
   public void loaded() {
     this.setStatus(Status.Server.SERVICE);
+  }
+
+  public WaitingGameManager getWaitingGameManager() {
+    return waitingGameManager;
   }
 }
